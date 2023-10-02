037ede307a321f98cb1296a3ee5d2cb2a8771c28
samayounis <your@email.here>
2023-10-01 20:30:33 -0400

    search functionality and account auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import {createGlobalStyle} from "styled-components";
 import {CartContextProvider} from "@/components/CartContext";
+import {SessionProvider} from "next-auth/react";
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
@@ -9,15 +10,22 @@ const GlobalStyles = createGlobalStyle`
     margin:0;
     font-family: 'Poppins', sans-serif;
   }
+  hr{
+    display: block;
+    border:0;
+    border-top:1px solid #ccc;
+  }
 `;
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps: {session, ...pageProps } }) {
   return (
     <>
       <GlobalStyles />
-      <CartContextProvider>
-        <Component {...pageProps} />
-      </CartContextProvider>
+      <SessionProvider session={session}>
+        <CartContextProvider>
+          <Component {...pageProps} />
+        </CartContextProvider>
+      </SessionProvider>
     </>
   );
 }
---
+++ pages/account.js
@@ -0,0 +1,141 @@
+import Header from "@/components/Header";
+import Title from "@/components/Title";
+import Center from "@/components/Center";
+import {signIn, signOut, useSession} from "next-auth/react";
+import Button from "@/components/Button";
+import styled from "styled-components";
+import WhiteBox from "@/components/WhiteBox";
+import {RevealWrapper} from "next-reveal";
+import Input from "@/components/Input";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Spinner from "@/components/Spinner";
+
+const ColsWrapper = styled.div`
+  display:grid;
+  grid-template-columns: 1.2fr .8fr;
+  gap: 40px;
+  margin: 40px 0;
+  p{
+    margin:5px;
+  }
+`;
+
+const CityHolder = styled.div`
+  display:flex;
+  gap: 5px;
+`;
+
+const WishedProductsGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 40px;
+`;
+
+export default function AccountPage() {
+  const {data:session} = useSession();
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [city,setCity] = useState('');
+  const [postalCode,setPostalCode] = useState('');
+  const [streetAddress,setStreetAddress] = useState('');
+  const [country,setCountry] = useState('');
+  const [loaded,setLoaded] = useState('false');
+  async function logout() {
+    await signOut({
+      callbackUrl: process.env.NEXT_PUBLIC_URL,
+    });
+  }
+  async function login() {
+    await signIn('google');
+  }
+  function saveAddress() {
+    const data = {name,email,city,streetAddress,postalCode,country};
+    axios.put('/api/address', data);
+  }
+  useEffect(() => {
+    axios.get('/api/address').then(response => {
+      setName(response.data.name);
+      setEmail(response.data.email);
+      setCity(response.data.city);
+      setPostalCode(response.data.postalCode);
+      setStreetAddress(response.data.streetAddress);
+      setCountry(response.data.country);
+      setLoaded(true);
+    });
+
+  }, []);
+  return (
+    <>
+      <Header />
+      <Center>
+        <ColsWrapper>
+          <div>
+            <RevealWrapper delay={0}>
+              <WhiteBox>
+                <h2>Wishlist</h2>
+              </WhiteBox>
+            </RevealWrapper>
+          </div>
+          <div>
+            <RevealWrapper delay={100}>
+              <WhiteBox>
+                <h2>Account Details</h2>
+                {!loaded && (
+                  <Spinner fullWidth={true} />
+                )}
+                {loaded && (
+                  <>
+                    <Input type="text"
+                           placeholder="Name"
+                           value={name}
+                           name="name"
+                           onChange={ev => setName(ev.target.value)} />
+                    <Input type="text"
+                           placeholder="Email"
+                           value={email}
+                           name="email"
+                           onChange={ev => setEmail(ev.target.value)}/>
+                    <CityHolder>
+                      <Input type="text"
+                             placeholder="City"
+                             value={city}
+                             name="city"
+                             onChange={ev => setCity(ev.target.value)}/>
+                      <Input type="text"
+                             placeholder="Postal Code"
+                             value={postalCode}
+                             name="postalCode"
+                             onChange={ev => setPostalCode(ev.target.value)}/>
+                    </CityHolder>
+                    <Input type="text"
+                           placeholder="Street Address"
+                           value={streetAddress}
+                           name="streetAddress"
+                           onChange={ev => setStreetAddress(ev.target.value)}/>
+                    <Input type="text"
+                           placeholder="Country"
+                           value={country}
+                           name="country"
+                           onChange={ev => setCountry(ev.target.value)}/>
+                    <Button black block
+                            onClick={saveAddress}>
+                      Save
+                    </Button>
+                    <hr/>
+                  </>
+                )}
+                {session && (
+                  <Button primary onClick={logout}>Logout</Button>
+                )}
+                {!session && (
+                  <Button primary onClick={login}>Login with Google</Button>
+                )}
+              </WhiteBox>
+            </RevealWrapper>
+          </div>
+        </ColsWrapper>
+      </Center>
+    </>
+  );
+}
