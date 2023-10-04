2e546f904184268e03b62f448743324d2d3a3718
samayounis <your@email.here>
2023-10-04 10:32:53 -0400

    order bug resolved

--- pages/account.js
+++ pages/account.js
@@ -1,13 +1,13 @@
 import Header from "@/components/Header";
 import Title from "@/components/Title";
 import Center from "@/components/Center";
-import {signIn, signOut, useSession} from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Button from "@/components/Button";
 import styled from "styled-components";
 import WhiteBox from "@/components/WhiteBox";
-import {RevealWrapper} from "next-reveal";
+import { RevealWrapper } from "next-reveal";
 import Input from "@/components/Input";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Spinner from "@/components/Spinner";
 import ProductBox from "@/components/ProductBox";
@@ -15,17 +15,17 @@ import Tabs from "@/components/Tabs";
 import SingleOrder from "@/components/SingleOrder";
 
 const ColsWrapper = styled.div`
-  display:grid;
-  grid-template-columns: 1.2fr .8fr;
+  display: grid;
+  grid-template-columns: 1.2fr 0.8fr;
   gap: 40px;
   margin: 40px 0;
-  p{
-    margin:5px;
+  p {
+    margin: 5px;
   }
 `;
 
 const CityHolder = styled.div`
-  display:flex;
+  display: flex;
   gap: 5px;
 `;
 
@@ -36,18 +36,18 @@ const WishedProductsGrid = styled.div`
 `;
 
 export default function AccountPage() {
-  const {data:session} = useSession();
-  const [name,setName] = useState('');
-  const [email,setEmail] = useState('');
-  const [city,setCity] = useState('');
-  const [postalCode,setPostalCode] = useState('');
-  const [streetAddress,setStreetAddress] = useState('');
-  const [country,setCountry] = useState('');
-  const [addressLoaded,setAddressLoaded] = useState(true);
-  const [wishlistLoaded,setWishlistLoaded] = useState(true);
-  const [orderLoaded,setOrderLoaded] = useState(true);
-  const [wishedProducts,setWishedProducts] = useState([]);
-  const [activeTab, setActiveTab] = useState('Orders');
+  const { data: session } = useSession();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
+  const [country, setCountry] = useState("");
+  const [addressLoaded, setAddressLoaded] = useState(true);
+  const [wishlistLoaded, setWishlistLoaded] = useState(true);
+  const [orderLoaded, setOrderLoaded] = useState(true);
+  const [wishedProducts, setWishedProducts] = useState([]);
+  const [activeTab, setActiveTab] = useState("Orders");
   const [orders, setOrders] = useState([]);
 
   async function logout() {
@@ -55,13 +55,16 @@ export default function AccountPage() {
       callbackUrl: process.env.NEXT_PUBLIC_URL,
     });
   }
+
   async function login() {
-    await signIn('google');
+    await signIn("google");
   }
+
   function saveAddress() {
-    const data = {name,email,city,streetAddress,postalCode,country};
-    axios.put('/api/address', data);
+    const data = { name, email, city, streetAddress, postalCode, country };
+    axios.put("/api/address", data);
   }
+
   useEffect(() => {
     if (!session) {
       return;
@@ -69,7 +72,7 @@ export default function AccountPage() {
     setAddressLoaded(false);
     setWishlistLoaded(false);
     setOrderLoaded(false);
-    axios.get('/api/address').then(response => {
+    axios.get("/api/address").then((response) => {
       setName(response.data.name);
       setEmail(response.data.email);
       setCity(response.data.city);
@@ -78,68 +81,87 @@ export default function AccountPage() {
       setCountry(response.data.country);
       setAddressLoaded(true);
     });
-    axios.get('/api/wishlist').then(response => {
-      setWishedProducts(response.data.map(wp => wp.product));
+    axios.get("/api/wishlist").then((response) => {
+      setWishedProducts(response.data.map((wp) => wp.product));
       setWishlistLoaded(true);
     });
-    axios.get('/api/orders').then(response => {
+    axios.get("/api/orders").then((response) => {
       setOrders(response.data);
       setOrderLoaded(true);
     });
   }, [session]);
+
   function productRemovedFromWishlist(idToRemove) {
-    setWishedProducts(products => {
-      return [...products.filter(p => p._id.toString() !== idToRemove)];
+    setWishedProducts((products) => {
+      return [...products.filter((p) => p._id.toString() !== idToRemove)];
     });
   }
+
   return (
     <>
-      <Header />
+      <Header key={new Date().getTime()} />
       <Center>
         <ColsWrapper>
           <div>
             <RevealWrapper delay={0}>
               <WhiteBox>
                 <Tabs
-                  tabs={['Orders','Wishlist']}
+                  tabs={["Orders", "Wishlist"]}
                   active={activeTab}
                   onChange={setActiveTab}
                 />
-                {activeTab === 'Orders' && (
+                {activeTab === "Orders" && (
                   <>
-                    {!orderLoaded && (
-                      <Spinner fullWidth={true} />
-                    )}
+                    {!orderLoaded && <Spinner fullWidth={true} />}
                     {orderLoaded && (
                       <div>
-                        {orders.length === 0 && (
-                          <p>No orders found</p>
-                        )}
-                        {!session && (
+                        {session ? (
+                          orders.length === 0 ? (
+                            <p>No orders found</p>
+                          ) : (
+                            <div>
+                              {orders.map((order, index) => (
+                                <SingleOrder
+                                  key={index} // Use index as the key if no unique identifier is available
+                                  createdAt={order.createdAt}
+                                  line_items={order.line_items}
+                                  name={name}
+                                  email={email}
+                                  streetAddress={streetAddress}
+                                  postalCode={postalCode}
+                                  city={city}
+                                  country={country}
+                                />
+                              ))}
+                            </div>
+                          )
+                        ) : (
                           <p>Login to see your orders</p>
                         )}
                       </div>
                     )}
                   </>
                 )}
-                {activeTab === 'Wishlist' && (
+                {activeTab === "Wishlist" && (
                   <>
-                    {!wishlistLoaded && (
-                      <Spinner fullWidth={true} />
-                    )}
+                    {!wishlistLoaded && <Spinner fullWidth={true} />}
                     {wishlistLoaded && (
                       <>
                         <WishedProductsGrid>
-                          {wishedProducts.length > 0 && wishedProducts.map(wp => (
-                            <ProductBox key={wp._id} {...wp} wished={true} onRemoveFromWishlist={productRemovedFromWishlist} />
+                          {wishedProducts.map((wp) => (
+                            <ProductBox
+                              key={wp._id}
+                              {...wp}
+                              wished={true}
+                              onRemoveFromWishlist={productRemovedFromWishlist}
+                            />
                           ))}
                         </WishedProductsGrid>
                         {wishedProducts.length === 0 && (
                           <>
-                            {session && (
+                            {session ? (
                               <p>Your wishlist is empty</p>
-                            )}
-                            {!session && (
+                            ) : (
                               <p>Login to add products to your wishlist</p>
                             )}
                           </>
@@ -154,57 +176,62 @@ export default function AccountPage() {
           <div>
             <RevealWrapper delay={100}>
               <WhiteBox>
-                <h2>{session ? 'Account details' : 'Login'}</h2>
-                {!addressLoaded && (
-                  <Spinner fullWidth={true} />
-                )}
+                <h2>{session ? "Account details" : "Login"}</h2>
+                {!addressLoaded && <Spinner fullWidth={true} />}
                 {addressLoaded && session && (
                   <>
-                    <Input type="text"
-                           placeholder="Name"
-                           value={name}
-                           name="name"
-                           onChange={ev => setName(ev.target.value)} />
-                    <Input type="text"
-                           placeholder="Email"
-                           value={email}
-                           name="email"
-                           onChange={ev => setEmail(ev.target.value)}/>
+                    <Input
+                      type="text"
+                      placeholder="Name"
+                      value={name}
+                      name="name"
+                      onChange={(ev) => setName(ev.target.value)}
+                    />
+                    <Input
+                      type="text"
+                      placeholder="Email"
+                      value={email}
+                      name="email"
+                      onChange={(ev) => setEmail(ev.target.value)}
+                    />
                     <CityHolder>
-                      <Input type="text"
-                             placeholder="City"
-                             value={city}
-                             name="city"
-                             onChange={ev => setCity(ev.target.value)}/>
-                      <Input type="text"
-                             placeholder="Postal Code"
-                             value={postalCode}
-                             name="postalCode"
-                             onChange={ev => setPostalCode(ev.target.value)}/>
+                      <Input
+                        type="text"
+                        placeholder="City"
+                        value={city}
+                        name="city"
+                        onChange={(ev) => setCity(ev.target.value)}
+                      />
+                      <Input
+                        type="text"
+                        placeholder="Postal Code"
+                        value={postalCode}
+                        name="postalCode"
+                        onChange={(ev) => setPostalCode(ev.target.value)}
+                      />
                     </CityHolder>
-                    <Input type="text"
-                           placeholder="Street Address"
-                           value={streetAddress}
-                           name="streetAddress"
-                           onChange={ev => setStreetAddress(ev.target.value)}/>
-                    <Input type="text"
-                           placeholder="Country"
-                           value={country}
-                           name="country"
-                           onChange={ev => setCountry(ev.target.value)}/>
-                    <Button black block
-                            onClick={saveAddress}>
+                    <Input
+                      type="text"
+                      placeholder="Street Address"
+                      value={streetAddress}
+                      name="streetAddress"
+                      onChange={(ev) => setStreetAddress(ev.target.value)}
+                    />
+                    <Input
+                      type="text"
+                      placeholder="Country"
+                      value={country}
+                      name="country"
+                      onChange={(ev) => setCountry(ev.target.value)}
+                    />
+                    <Button black block onClick={saveAddress}>
                       Save
                     </Button>
-                    <hr/>
+                    <hr />
                   </>
                 )}
-                {session && (
-                  <Button primary onClick={logout}>Logout</Button>
-                )}
-                {!session && (
-                  <Button primary onClick={login}>Login with Google</Button>
-                )}
+                {session && <Button primary onClick={logout}>Logout</Button>}
+                {!session && <Button primary onClick={login}>Login with Google</Button>}
               </WhiteBox>
             </RevealWrapper>
           </div>
